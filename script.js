038444c6c54fe6fc9c90486b96915ea14e74c23d
sean innella <sean.innella@gmail.com>
2023-10-21 16:20:28 -0400

    Added final JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
 
 function hoverOverZelda(img) {
     img.src = "images/Ign10.gif";
