ca4b1a3252b6639ae2935f7a2fb95a6e1d83f624
sean innella <sean.innella@gmail.com>
2023-10-21 15:05:34 -0400

    Adding hoverOver and hoverOff cunctions to game images

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+function hoverOverZelda(img) {
+    img.src = "images/Ign10.gif";
+}
+
+function hoverOutZelda(img) {
+    img.src = "images/ZeldaTearsOfTheKingdom.jpeg";
+}
+
+function hoverOverVampireSurvivors(img) {
+    img.src = "images/Ign10.gif";
+}
+
+function hoverOutVampireSurvivors(img) {
+    img.src = "images/VampireSurvivors.jpeg";
+}
+
+function hoverOverStarfield(img) {
+    img.src = "images/Ign8.gif";
+}
+
+function hoverOutStarfield(img) {
+    img.src = "images/Starfield.jpeg";
+}
